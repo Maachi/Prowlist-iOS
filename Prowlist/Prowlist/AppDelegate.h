6a2c009a4be2194ce76803b326f1d1850ecae808
Sebastian Romero <bipsa81@gmail.com>
2015-05-06 12:57:39 -0500

    First commit

---
+++ Prowlist/Prowlist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prowlist
+//
+//  Created by Sebastian Romero on 5/6/15.
+//  Copyright (c) 2015 Maachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
