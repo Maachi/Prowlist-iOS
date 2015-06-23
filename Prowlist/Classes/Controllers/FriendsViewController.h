e75906d65f7364f6c7a189385250e2fc3c25409c
Laura Martinez <laucmar@gmail.com>
2015-06-22 21:35:47 -0500

    First Friends View Controller version

---
+++ Prowlist/Classes/Controllers/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  Prowlist
+//
+//  Created by Laura C. Martinez on 6/22/15.
+//  Copyright (c) 2015 Maachi. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface FriendsViewController : BaseViewController
+
+@end
